c701e0de258e8b8e1e08496ecba72e485c42a465
zhung1oz7 <55905379+wl00494849@users.noreply.github.com>
2021-05-14 17:15:34 +0800

    channel Test

--- Server/channel.go
+++ Server/channel.go
@@ -4,26 +4,35 @@ import (
 	"fmt"
 )
 
-var channl chan string
+var channel chan string
+var channel1 chan string
 var sli []string
 
 func ChanTest(msg []string) []string {
-	channl = make(chan string)
+	channel = make(chan string)
+	channel1 = make(chan string)
 	sli = make([]string, 0)
 
 	go chanOutPut()
+	go chanPrint()
 
 	for _, ch := range msg {
 		fmt.Println(ch)
-		channl <- ch
+		channel <- ch
 	}
 
 	return sli
 }
 
 func chanOutPut() {
-	for ch := range channl {
-		fmt.Printf("add %s", ch)
+	for ch := range channel {
 		sli = append(sli, "Say "+ch)
+		channel1 <- ch
+	}
+}
+
+func chanPrint() {
+	for ch := range channel1 {
+		fmt.Println("Add " + ch)
 	}
 }
