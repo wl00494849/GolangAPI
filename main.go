71cf9f7ee48cd2163fb0a4cbdc87a973c3928246
AKA迪迪弄 <wl00494849@gmail.com>
2021-06-16 15:00:47 +0800

    break

--- main.go
+++ main.go
@@ -28,11 +28,6 @@ func main() {
 	//監聽Port設定
 	err := http.ListenAndServe(":8778", c.Handler(mux))
 	er.CheckErr(err)
-
-	// http.HandleFunc("/SocketPage", controller.SockretPage)
-	// http.Handle("/Socket", websocket.Handler(server.Echo))
-	// http.ListenAndServe(":8877", nil)
-
 }
 
 //路由
@@ -42,19 +37,14 @@ func (m Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/CreateUser":
 		controller.CreateUser(w, r)
-		break
 	case "/UsersList":
 		controller.UsersList(w, r)
-		break
 	case "/DockerTest":
 		controller.DockerTest(w, r)
-		break
 	case "/DeleteUser":
 		controller.DeleteUser(w, r)
-		break
 	case "/ChannlTest":
 		controller.ChannelTest(w, r)
-		break
 	case "/SocketPage":
 		controller.SockretPage(w, r)
 	default:
